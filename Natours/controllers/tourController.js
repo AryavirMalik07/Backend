a0309132dc44184309d9157f8de274bced397a7c
AryavirMalik07 <aryavirmalik20@gmail.com>
2023-03-15 23:05:35 +0530

    filter aliasing

--- Natours/controllers/tourController.js
+++ Natours/controllers/tourController.js
@@ -1,6 +1,7 @@
 const { match } = require("assert");
 const fs = require("fs");
 const Tour = require("./../models/tourModel");
+const APIFeatures = require("./../utils/apiFeatures");
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
@@ -54,60 +55,13 @@ exports.aliasTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    // Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
-    console.log(req.query, queryObj);
-    // 1st by using filter object
-    // const tours = await Tour.find({
-    //   duration: 5,
-    //   difficulty: "easy",
-    // });
-    // Advance Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
-
-    // 3rd way
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2nd way
-    // const tours = await Tour.find()
-    //   .where("duration")
-    //   .equals(5)
-    //   .where("difficulty")
-    //   .equals("easy");
-
-    // SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt");
-    }
-
-    // Field Limit
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v");
-    }
-
-    // Pagination*********
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    // pag2&&limit10 means 1-10 page 1,11-20 page 2,21-30 page 3
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error("this page doesnt exist");
-    }
     // Excecute the query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
     res.status(200).json({
       status: "success",
       requestedAt: req.requestTime,
@@ -207,3 +161,56 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+// Filtering
+// const queryObj = { ...req.query };
+// const excludedFields = ["page", "sort", "limit", "fields"];
+// excludedFields.forEach((el) => delete queryObj[el]);
+// console.log(req.query, queryObj);
+// // 1st by using filter object
+// // const tours = await Tour.find({
+// //   duration: 5,
+// //   difficulty: "easy",
+// // });
+// // Advance Filtering
+// let queryStr = JSON.stringify(queryObj);
+// queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+// console.log(JSON.parse(queryStr));
+
+// // 3rd way
+// let query = Tour.find(JSON.parse(queryStr));
+
+// 2nd way
+// const tours = await Tour.find()
+//   .where("duration")
+//   .equals(5)
+//   .where("difficulty")
+//   .equals("easy");
+
+// SORTING
+// if (req.query.sort) {
+//   const sortBy = req.query.sort.split(",").join(" ");
+//   query = query.sort(sortBy);
+// } else {
+//   query = query.sort("-createdAt");
+// }
+
+// Field Limit
+// if (req.query.fields) {
+//   const fields = req.query.fields.split(",").join(" ");
+//   query = query.select(fields);
+// } else {
+//   query = query.select("-__v");
+// }
+
+// Pagination*********
+// const page = req.query.page * 1 || 1;
+// const limit = req.query.limit * 1 || 100;
+// const skip = (page - 1) * limit;
+// // pag2&&limit10 means 1-10 page 1,11-20 page 2,21-30 page 3
+// query = query.skip(skip).limit(limit);
+
+// if (req.query.page) {
+//   const numTours = await Tour.countDocuments();
+//   if (skip >= numTours) throw new Error("this page doesnt exist");
+// }
