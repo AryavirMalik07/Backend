9e264841c058f86f86a9d34bc510ffba7b6fbd40
AryavirMalik07 <aryavirmalik20@gmail.com>
2023-03-22 23:09:25 +0530

    authenticatin jwt bcrypt

---
+++ Natours-2/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const morgan = require("morgan");
+const app = express();
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+
+// 1ST MIDDLEWARE
+app.use(morgan("dev"));
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use(express.json()); //middleware
+
+app.use((req, res, next) => {
+  console.log("hello middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  console.log(req.headers);
+  next();
+});
+
+app.use("/api/v1/tours", tourRouter);
+
+app.use("/api/v1/users", userRouter);
+
+app.listen(3001, () => {
+  console.log("port on 3001");
+});
+
+module.exports = app;
---
+++ Natours-2/server.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log("database connected");
+  });
+
+// adding data in our db
+
+// const testTour = new Tour({
+//   name: "the uttarPradesh hicker",
+//   rating: 4.7,
+//   price: 510,
+// });
+
+// testTour
+//   .save()
+//   .then((doc) => {
+//     console.log(doc);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
